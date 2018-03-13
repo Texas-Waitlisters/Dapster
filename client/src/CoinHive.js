0e44636e2918bc4102f6b638b26dafc5ecf73b33
Robert Clendenin <clendenin@utexas.edu>
2018-03-13 17:21:19 -0500

    background change

--- client/src/CoinHive.js
+++ client/src/CoinHive.js
@@ -13,7 +13,8 @@ export default class extends React.Component {
              data-whitelabel = "false"
              data-throttle = "1" //50 percent thread utilization
              data-threads = "5"
-             data-background = "#A9A9A9" //Dark grey
+             //data-background = "#A9A9A9" //Dark grey
+             data-background = "#ffffff"
              data-action = "0099cc" //Teal
              data-graph = "0099cc"
              data-text = "0099cc">
