c9e7d76bf73597befa7a6bc729393433384fd0e0
copperstick6 <copperstick6@gmail.com>
2018-03-13 17:36:31 -0500

    fixed spacing issue on nav bar, added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,28 @@
 import React, { Component } from 'react';
 
-import FacebookLogin from './FacebookLogin';
-
 import Header from './Header';
-import StreamQueue from './StreamQueue';
-import Profile from './Profile';
-
-import CoinHive from './CoinHive';
+import DefaultRouter from './Router'
 
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      currentSong: null,
-      totalHashes: 0,
-      songHashes: 0
-    }
+	super(props);
+	this.state = {
+	  currentSong: null,
+	  totalHashes: 0,
+	  songHashes: 0
+	}
   }
 
   render() {
-    return (
-      <div className="App">
-        <Header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Dapster</h1>
-        </header>
-        {/* if url=/profile add artis details */}
-        <CoinHive />
-        <div id="content-container">
-          <StreamQueue />
-          <Profile />
-        </div>
-        <FacebookLogin />
-      </div>
-    );
+	return (
+	  <div className="App">
+		<Header />
+		<DefaultRouter />
+	  </div>
+	);
   }
 }
 
