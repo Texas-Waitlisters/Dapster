a703c3df0d5c233b5505dd8e38d047f3f1786332
Robert Clendenin <clendenin@utexas.edu>
2018-03-13 15:11:33 -0500

    Cusotm coinHive code

--- client/src/CoinHive.js
+++ client/src/CoinHive.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import CoinHive from 'react-coinhive';
- 
+
 export default class extends React.Component {
   render() {
     return (
-      <CoinHive
-        userName="Maya" // username would be artists username so would go to their wallet associated with our website
-        siteKey="caP8U8pZXH6n0f53eV3fdpwOvpmTAD3C"
-        autoThreads={false}
-        threads={2}
-        src={CoinHive.src.authedmine}
-        onInit={(miner) => setInterval(
-          () => console.log(CoinHive.getMinerData(miner))
-          , 1000
-        )}
-        onStop={() => {
-
-        }}
-      />
+        <script src="https://authedmine.com/lib/simple-ui.min.js" async></script>
+        <div class="coinhive-miner"
+  	         style="width: 256px; height: 310px"
+  	         data-key="YOUR_SITE_KEY">
+  	         <em>Loading...</em>
+        </div>
     )
   }
 }
