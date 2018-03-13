6fcc02b76b290f1e98598ab48a8be4108f3f9048
Kiba Gateaux <kibagateaux@gmail.com>
2018-03-13 16:49:44 -0500

    fixes minor UI bugs

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -4,6 +4,7 @@ export default (props) => {
   const renderUserProfile = () => {
     // localStorage.getItem('facebookID');
     // facebookAPI.getUser(facebookID)
+    const totalCoins = Number(props.totalHashes) ? props.totalHashes * 50 : 500;
     return (
       <div>
         <h4> USER PROFILE </h4>
@@ -11,18 +12,19 @@ export default (props) => {
           <h1> My name </h1>
           {/* <img src={facebookImg} */}
         </div>
-        <h4> Dappster Coins Earned: {props.totalHashes * 50} </h4> 
+        <h4> Dappster Coins Earned: {totalCoins} </h4> 
       </div>
     );
   }
 
   const renderArtistProfile = (artistID) => {
     // cloudinary.get(artistID)
+    const totalCoins = Number(props.totalHashes) ? props.totalHashes * 200 : 2000;
     return (
       <div>
         <h4> ARTIST PROFILE </h4>
         {/* <StreamQueue songs={artistSongs} */}
-        <h4> Total Dappsters earned: {props.totalHashes * 200} </h4>
+        <h4> Total Dappsters earned: {totalCoins} </h4>
       </div>
     );
   }
