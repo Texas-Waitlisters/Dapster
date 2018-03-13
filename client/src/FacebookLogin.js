95bd0b52a3febccda1d62fa9c949d4127e0eadc5
Kiba Gateaux <kibagateaux@gmail.com>
2018-03-13 13:07:13 -0500

    adds react web client

---
+++ client/src/FacebookLogin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class FacebookLogin extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1> Lgin with Faceboook </h1>
+      </div>
+    );
+  }
+}
+
+
