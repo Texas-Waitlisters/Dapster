7ce854793ec1b17a5b65fb527c30e7707105ccfd
Robert Clendenin <clendenin@utexas.edu>
2018-03-13 15:27:28 -0500

    Prefs

--- client/src/CoinHive.js
+++ client/src/CoinHive.js
@@ -5,14 +5,19 @@ export default class extends React.Component {
   render() {
     return (
         <script src="https://authedmine.com/lib/simple-ui.min.js" async></script>
-        <div class="coinhive-miner"
-  	         style="width: 256px; height: 310px"
-  	         data-key="iUHtfh5ZLhgoaOUoraaCbzwVlGhJkPWt">
-             data-user="Dappster Music Distribution"
+        <div class = "coinhive-miner"
+  	         style = "width: 256px; height: 310px"
+  	         data-key = "iUHtfh5ZLhgoaOUoraaCbzwVlGhJkPWt">
+             data-user = "Dappster Music Distribution"
              //data-user Should be converted to current musicians name
-             data-autostart="true" //Automatically starts mining when user logs in
-             data-whitelabel="false"
-  	         <em>Loading...</em>
+             data-autostart = "true" //Automatically starts mining when user logs in
+             data-whitelabel = "false"
+             data-throttle = ".5" //50 percent thread utilization
+             data-threads = "3"
+             data-background = "#D3D3D3" //Grey
+             data-action="008080" //Teal
+             >
+  	         <em>Mining...</em>
         </div>
     )
   }
