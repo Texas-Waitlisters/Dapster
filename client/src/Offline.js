bfaa1efa53eba6336fd39591958e84fffcbab166
copperstick6 <copperstick6@gmail.com>
2018-03-13 20:07:43 -0500

    merge fix

--- client/src/Offline.js
+++ client/src/Offline.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react'
+import { RingLoader } from 'react-spinners';
 
 export default class Offline extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			loaded: true
+		}
+	}
+
+	componentWillMount(){
+
+	}
 	render(){
 		return(
 			<div>
+			<RingLoader
+			  color={'#123abc'}
+			  loading={this.state.loading}
+			/>
 			<p>Hi</p>
 			</div>
 		)
