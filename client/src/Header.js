485f9e9d69b8f994addbf6147a12884f25569643
Kiba Gateaux <kibagateaux@gmail.com>
2018-03-13 20:46:56 -0500

    styles code formatting

--- client/src/Header.js
+++ client/src/Header.js
@@ -16,35 +16,37 @@ export default class Header extends Component{
 		}
 		this.handleHomeClick = this.handleHomeClick.bind(this)
 	}
+
 	handleHomeClick(){
 		this.setState({open: false})
-
 	}
 
 	render(){
 	  return (
-
 		  <AppBar position="static">
-			<Toolbar>
-			  <IconButton className="menuButton" color="inherit" aria-label="Menu">
-				<MenuIcon onClick = {() => this.setState({open: !this.state.open})}/>
-				<Drawer
-				  docked={false}
-				  width={200}
-				  open={this.state.open}
-				  onRequestChange={(open) => this.setState({open})}
-				>
-				  <Link to="/"><MenuItem onClick={() => this.setState({open: false})}>Home</MenuItem></Link>
-				  <Link to="/offline"><MenuItem onClick={() => this.setState({open: false})}>Offline Viewing</MenuItem></Link>
-				</Drawer>
-				</IconButton>
-			  <Typography variant="title" color="inherit">
-				Dappster &nbsp; &nbsp;
-			  </Typography>
-			  <FacebookLogin />
-			</Toolbar>
-		  </AppBar>
+				<Toolbar>
 
+					<IconButton className="menuButton" color="inherit" aria-label="Menu">
+						<MenuIcon onClick = {() => this.setState({open: !this.state.open})}/>
+						<Drawer
+							docked={false}
+							width={200}
+							open={this.state.open}
+							onRequestChange={(open) => this.setState({open})}
+						>
+							<Link to="/"><MenuItem onClick={() => this.setState({open: false})}>Home</MenuItem></Link>
+							<Link to="/offline"><MenuItem onClick={() => this.setState({open: false})}>Offline Viewing</MenuItem></Link>
+						</Drawer>
+					</IconButton>
+
+					<Typography variant="title" color="inherit">
+						Dappster &nbsp; &nbsp;
+					</Typography>
+
+					<FacebookLogin />
+					
+				</Toolbar>
+		  </AppBar>
 	  );
 	}
 }
--- client/src/Profile.js
+++ client/src/Profile.js
@@ -12,17 +12,21 @@ export default class extends Component {
   }
 
   renderUserProfile = () => {
-    // localStorage.getItem('facebookID');
-    // facebookAPI.getUser(facebookID)
+
     const totalCoins = Number(this.props.totalHashes) ? this.props.totalHashes * 50 : 500;
     return (
       <div>
+
         <h4> USER PROFILE </h4>
+        
         <div>
-          <h1> {localStorage.getItem('username')} </h1>
-          <img src={localStorage.getItem('profileImage')} />
+          <h1> {localStorage.getItem('username') || "Login to Facebook :P"} </h1>
+          {localStorage.getItem('username') && 
+          <img className="profile-pic" src={localStorage.getItem('profileImage')} />}
         </div>
+        
         <h4> Dappster Coins Earned: {totalCoins} </h4> 
+      
       </div>
     );
   }
@@ -31,37 +35,39 @@ export default class extends Component {
     if(this.state.type === "artist") {
       const totalCoins = Number(this.props.totalHashes) ? this.props.totalHashes * 200 : 2000;
       const {releases} = this.state;
-      console.log('art prof', this.state);
       return (
         <div>
           <h1> {releases[0].artist ? releases[0].artist.name : "Travis 'The Greatest' Laurendine"} </h1>
+          
           <h4> ARTIST PROFILE </h4>            
           <h4> Total Dappsters earned: {totalCoins} </h4>
+
           <h4> Notable Releasess: {releases ? 
             releases.map((r) => `${r.type} - ${r.title}`) :
             "This artists has done too many amazing things to list"}
           </h4>
+
         </div>
       );
     } else {
-        const artistProfile = axios
-        .get(`https://evangelism.cloudinary.auth0-extend.com/sxsw-music-discovery-service/releases/${artistID}`)
-          .then((data) => {
-            const { releases: { release } } = data;
-            this.setState({
-              type: "artist",
-              releases: release
-            })
-            console.log('release', release);
-          })
-          .catch((err) => {
-            this.setState({
-              type: "artist",
-              releases: [{type: "StackOverflow", title: "This artists has done too many amazing things to list"}]
-            });
-          });
-          console.log('art prof', artistProfile);
-      }
+      const artistProfile = axios
+      .get(`https://evangelism.cloudinary.auth0-extend.com/sxsw-music-discovery-service/releases/${artistID}`)
+      .then((data) => {
+        const { releases: { release } } = data;
+        this.setState({
+          type: "artist",
+          releases: release
+        })
+        console.log('release', release);
+      })
+      .catch((err) => {
+        this.setState({
+          type: "artist",
+          releases: [{type: "StackOverflow", title: "This artists has done too many amazing things to list"}]
+        });
+      });
+      console.log('art prof', artistProfile);
+    }
   }
 
   render() {
